94fc1042b01a1e3e92ae57f1f7df2415de1c1ec0
GUSTAVO GOMES FERREIRA <gustavo.gferreira@aluno.senac.rio>
2026-03-16 21:31:37 -0300

    Adicionando comentários

--- TESTE DE MESA/ATIVIDADE4.py
+++ TESTE DE MESA/ATIVIDADE4.py
@@ -3,28 +3,28 @@
 
 valor = int(input("Digite o valor a ser sacado: ")) # imputando valor a ser sacado...
 
-while valor >= 200:
+while valor >= 200: # Verificando se a nota é maior que 200...
     print("Nota de R$200")
     valor -= 200    
-while valor >= 100:
+while valor >= 100: # Verificando se a nota é maior que 100...
     print("Nota de R$100")
     valor -= 100
-while valor >= 50:
+while valor >= 50: # Verificando se a nota é maior que 50...
     print("Nota de R$50")
     valor -= 50
-while valor >= 20:
+while valor >= 20: # Verificando se a nota é maior que  20...
     print("Nota de R$20")
     valor -= 20
-while valor >= 10:
+while valor >= 10: # Verificando se a nota é maior que 10...
     print("Nota de R$10")
     valor -= 10
-while valor >= 5:
+while valor >= 5: # Verificando se a nota é maior que 5...
     print("Nota de R$5")
     valor -= 5
-while valor >= 2:
+while valor >= 2: # Verificando se a nota é maior que 2...
     print("Nota de R$2")
     valor -= 2
-while valor >= 1:
+while valor >= 1: # Verificando se a moeda é maior que 1...
     print("Moeda de R$1")
     valor -= 1
     
